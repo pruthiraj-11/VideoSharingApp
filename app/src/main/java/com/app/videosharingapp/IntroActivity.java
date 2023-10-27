1524375daf1bcc6324ecd269e769cde26987c5e7
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-10-27 11:37:13 +0530

    Building YSC

--- app/src/main/java/com/app/videosharingapp/IntroActivity.java
+++ app/src/main/java/com/app/videosharingapp/IntroActivity.java
@@ -1,7 +1,6 @@
 package com.app.videosharingapp;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -10,6 +9,7 @@ import android.provider.Settings;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.videosharingapp.databinding.ActivityIntroBinding;
